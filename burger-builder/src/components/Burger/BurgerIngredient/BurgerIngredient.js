102c89879139394a0edc2864e0d51ad744a76f12
Bonny619 <samalanubhav1@yahoo.in>
2021-02-27 14:29:03 +0530

    installed prop-types package and added validation for type props

--- burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ burger-builder/src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './BurgerIngredient.module.css'
+import PropTypes from 'prop-types';
 
 const burgerIngredient = props => {
     let ingredient = null;
@@ -31,6 +32,10 @@ const burgerIngredient = props => {
             ingredient = null;
     }
     return ingredient;
+}
+
+burgerIngredient.propTypes = {
+    type: PropTypes.string.isRequired
 }
 
 export default burgerIngredient;
