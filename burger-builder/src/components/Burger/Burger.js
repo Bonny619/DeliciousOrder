81cc0be2bb04ff420aa10f6e6d52e651759566dd
Bonny619 <samalanubhav1@yahoo.in>
2021-03-01 10:25:32 +0530

    added validation message in case no ingredients added

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,14 +3,15 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = props => {
-    const transformedIngredients = Object.keys(props.ingredients).map(
+    let transformedIngredients = Object.keys(props.ingredients).map(
         igKey => {
             return [...Array(props.ingredients[igKey])].map(
                 (_,i) => { return <BurgerIngredient key={igKey + i} type={igKey}/>;                     
-                }
-            );
+            });
+        }).reduce((cur,el) => cur.concat(el), []);
+        if (transformedIngredients.length === 0) {
+            transformedIngredients = <p>Please add ingredients</p>            
         }
-    );
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
