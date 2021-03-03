7105a9a57ba7ad463342c8485bd63b91ad0b1f4b
Bonny619 <samalanubhav1@yahoo.in>
2021-03-03 11:17:18 +0530

    Added price

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -9,14 +9,16 @@ const controls = [
     {label: 'Meat', type: 'meat'}
 ]
 
-const buildControls = props => (
-    <div className={classes.buildControls}>
+const buildControls = props => (  
+    <div className={classes.buildControls}>   
+    <p>Total Price: <strong>{props.price.toFixed(2)}</strong></p>     
         {controls.map(ctrl => <BuildControl 
             key={ctrl.label} 
             label={ctrl.label} 
             added={() => props.ingredientAdded(ctrl.type)}
             removed={() => props.ingredientRemoved(ctrl.type)}
-            disabled={props.disabled[ctrl.type]}/>)}            
+            disabled={props.disabled[ctrl.type]}/>)}
+                
     </div>
 );
 
